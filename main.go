059ad7ca9a4ea2ab819d3d8b28fd1c1af0ad06f6
Brad Smith <brad.alan.smith@gmail.com>
2016-05-11 10:29:12 -0700

    now with logging!

--- main.go
+++ main.go
@@ -2,9 +2,17 @@ package main
 
 import (
     "fmt"
+    "log"
     "net/http"
     "encoding/json"
 )
+
+func Log(handler http.Handler) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        log.Printf("%s %s %s", r.RemoteAddr, r.Method, r.URL)
+	handler.ServeHTTP(w, r)
+    })
+}
 
 func handler(w http.ResponseWriter, r *http.Request) {
     querystring, _ := json.Marshal(r.URL.Query())
@@ -13,7 +21,8 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+    fmt.Println("Serving echo on :8080")
     http.HandleFunc("/", handler)
     http.HandleFunc("/echo", handler)
-    http.ListenAndServe(":8080", nil)
+    http.ListenAndServe(":8080", Log(http.DefaultServeMux))
 }
