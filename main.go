a2afdf537d731f4592fc6061192a601c3a393c85
Brad Smith <brad.alan.smith@gmail.com>
2016-05-27 10:06:16 -0700

    all the verbs!

--- main.go
+++ main.go
@@ -9,5 +9,13 @@ func main() {
     r := gin.Default()
     r.GET("/", echgo.Echgo)
     r.GET("/echo", echgo.Echgo)
+    r.POST("/", echgo.Echgo)
+    r.POST("/echo", echgo.Echgo)
+    r.PUT("/", echgo.Echgo)
+    r.PUT("/echo", echgo.Echgo)
+    r.PATCH("/", echgo.Echgo)
+    r.PATCH("/echo", echgo.Echgo)
+    r.DELETE("/", echgo.Echgo)
+    r.DELETE("/echo", echgo.Echgo)
     r.Run() // listen and server on 0.0.0.0:8080
 }
